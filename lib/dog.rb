0c2de59e092bb71b1e22ef752ede3e9760463572
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-22 21:14:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,16 @@
 class Dog
-
+  attr_reader :name
   @@all = []
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+  
+  def all
+    puts @@all
+  end
+  
+  def clear_all
+    @@all = []
+  end
 end
